c56c1c0d9abc8f7f198e83c0b825e17beb885077
uzuran <archloverlinux@gmail.com>
2022-03-31 11:32:38 +0200

    Alien invasion !

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,1 +1,27 @@
 import pygame
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+from pygame.sprite import Group
+
+
+def run_game():
+    # Initialize game and create screen object
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien invasion")
+    ship = Ship(screen, ai_settings)
+    # Make a group to store bullets in
+    bullets = Group()
+
+    # Start the main loop for game.
+    while True:
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        bullets.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_bullets(bullets)
+
+
+run_game()
---
+++ alien_invasion/game_functions.py
@@ -0,0 +1,63 @@
+import pygame
+import sys
+from bullet import Bullet
+
+
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
+    """Respond to keypresses."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_SPACE:
+        fire_bullet(ai_settings, screen, ship, bullets)
+
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    # Create a new bullet and add it to the bullets group.
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
+
+
+def check_keyup_events(event, ship):
+    """Respond to key releases."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+
+def check_events(ai_settings, screen, ship, bullets):
+    """Respond to keypresses and mouse events."""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
+
+
+def update_screen(ai_settings, screen, ship, bullets):
+    # Redraw the screen during each pass through the loop.
+    screen.fill(ai_settings.bg_color)
+    # Add image all location are in ai_settings(settings.py)
+    screen.blit(ai_settings.background, (0, 0))
+    ship.blitme()
+
+    # Redraw all bullets behind ship and aliens
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+    # Make the most recently drawn screen visible.
+    pygame.display.flip()
+
+
+def update_bullets(bullets):
+    # Get rid of bullets that have disappeared.
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+
