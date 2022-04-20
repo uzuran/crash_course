cb8ce8ce865c5cc912204cde06efcce671489cff
uzuran <archloverlinux@gmail.com>
2022-04-20 12:09:36 +0200

    add

--- dog.py
+++ dog.py
@@ -1,5 +1,4 @@
 class Dog:
-
    def __init__(self, name, age):
        self.name = name
        self. age = age
@@ -8,7 +7,9 @@ class Dog:
        print(self.name.title() + ' is now sitting.')
 
    def roll_over(self):
+
         print(self.name + ' rolled over.')
+
 
 my_dog = Dog('While', 6)
 
